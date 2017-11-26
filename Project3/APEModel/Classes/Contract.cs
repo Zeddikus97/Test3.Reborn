1c4b2bf691e075c654715ebc5705b6e1227f0138
Zeddikus97 <opatriarkh@gmail.com>
2017-11-26 04:32:49 +0300

    Made event model of abonents connections

---
+++ Project3/APEModel/Classes/Contract.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project3.APEModel.Classes
+{
+    class Contract
+    {
+        public string Name { get; private set; }
+        public string Number { get; private set; }
+        public Contract(string name, string number)
+        {
+            Name = name;
+            Number = number;
+        }     
+    }
+}
